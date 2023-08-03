8d34d8837596fce55e735c1d30e786a110d79fea
Nelson Chung <chihchun.chung@gmail.com>
2023-08-04 06:55:03 +0800

    管理者登入後，可以在BossShowDriversPositionPage顯示司機的定位資訊

--- User/bitrental/lib/boss_show_drivers_position_page.dart
+++ User/bitrental/lib/boss_show_drivers_position_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
 class BossShowDriversPositionPage extends StatefulWidget {
@@ -12,155 +10,87 @@ class BossShowDriversPositionPage extends StatefulWidget {
   _BossShowDriversPositionPageState createState() => _BossShowDriversPositionPageState();
 }
 
-class _BossShowDriversPositionPageState extends State<BossShowDriversPositionPage> with WidgetsBindingObserver{
+class _BossShowDriversPositionPageState extends State<BossShowDriversPositionPage> {
   late GoogleMapController mapController;
-  Location location = Location();
-  LatLng? currentLocation;
-  double? bearing;
   static const LatLng taiwanLatLng = LatLng(25.0330, 121.5654);
-  StreamSubscription<LocationData>? locationSubscription;
+  Map<String, Marker> markers = {}; // 使用 Map 來保存司機標記，以司機 ID 為鍵
 
   @override
   void initState() {
     super.initState();
-    initLocation();
-    WidgetsBinding.instance.addObserver(this);
+    getDriversLocations();
   }
 
-  @override
-  void dispose() {
-    locationSubscription?.cancel();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
+  Future<void> getDriversLocations() async {
+    try {
+      // 從 Firebase 的 "locations" 集合中取得所有文件
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('locations').get();
 
-  void initLocation() async {
-    bool serviceEnabled;
-    PermissionStatus permissionGranted;
+      // 更新有變化的司機標記
+      querySnapshot.docs.forEach((doc) {
+        double latitude = doc['latitude'];
+        double longitude = doc['longitude'];
+        String driverId = doc['id'];
 
-    serviceEnabled = await location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return;
-      }
-    }
+        // 如果司機 ID 在 markers 中已存在，且座標有變化，則更新該司機的標記
+        if (markers.containsKey(driverId) && markers[driverId]!.position.latitude != latitude && markers[driverId]!.position.longitude != longitude) {
+          setState(() {
+            markers[driverId] = Marker(
+              markerId: MarkerId(driverId),
+              position: LatLng(latitude, longitude),
+              infoWindow: InfoWindow(
+                title: '司機 $driverId',
+                snippet: '緯度: $latitude, 經度: $longitude',
+              ),
+            );
+          });
 
-    permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    LocationData locationData = await location.getLocation();
-    LatLng initialLocation = LatLng(
-      locationData.latitude ?? taiwanLatLng.latitude,
-      locationData.longitude ?? taiwanLatLng.longitude,
-    );
-
-    mapController.moveCamera(CameraUpdate.newLatLngZoom(initialLocation, 15));
-
-    setState(() {
-      currentLocation = initialLocation;
-    });
-
-    startListening();
-  }
-
-  void startListening() {
-    locationSubscription?.cancel();
-    locationSubscription =
-        location.onLocationChanged.listen((LocationData currentLocation) {
-      setState(() {
-        this.currentLocation = LatLng(
-          currentLocation.latitude ?? taiwanLatLng.latitude,
-          currentLocation.longitude ?? taiwanLatLng.longitude,
-        );
-        bearing = currentLocation.heading;
+            // 顯示司機的資訊
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                    content: Text('司機 $driverId 的位置資訊已更新：緯度 $latitude, 經度 $longitude'),
+                    duration: Duration(seconds: 2),
+                ),
+            );
+        } else {
+          // 否則建立新的司機標記
+          markers[driverId] = Marker(
+            markerId: MarkerId(driverId),
+            position: LatLng(latitude, longitude),
+            infoWindow: InfoWindow(
+              title: '司機 $driverId',
+              snippet: '緯度: $latitude, 經度: $longitude',
+            ),
+          );
+        }
       });
-    });
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      startListening();
-    } else if (state == AppLifecycleState.paused) {
-      locationSubscription?.cancel();
+    } catch (error) {
+      print('Error occurred while getting drivers locations: $error');
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text('顯示司機位置'),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      /*
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/background.png'),
-            fit: BoxFit.cover,
-          ),
+      body: GoogleMap(
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
+        initialCameraPosition: CameraPosition(
+          target: taiwanLatLng,
+          zoom: 15,
         ),
-      ),
-      */
-      body: Stack(
-        children: [
-          GoogleMap(
-            onMapCreated: (controller) {
-              mapController = controller;
-            },
-            initialCameraPosition: CameraPosition(
-              target: currentLocation ?? taiwanLatLng,
-              zoom: 15,
-            ),
-            markers: <Marker>{
-              Marker(
-                markerId: const MarkerId('currentLocation'),
-                position: currentLocation ?? const LatLng(0, 0),
-                infoWindow: const InfoWindow(
-                  title: 'Current Location',
-                ),
-              ),
-            },
-            compassEnabled: true,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: false,
-            rotateGesturesEnabled: false,
-            tiltGesturesEnabled: false,
-            onCameraMove: (CameraPosition position) {
-              setState(() {
-                bearing = position.bearing;
-              });
-            },
-          ),
-          if (bearing != null)
-            Positioned(
-              top: 16,
-              right: 16,
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                child: Text(
-                  'Bearing: ${bearing!.toStringAsFixed(2)}°',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-        ],
+        markers: Set<Marker>.from(markers.values), // 將 markers 中的標記轉換成 Set<Marker>
+        compassEnabled: true,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        rotateGesturesEnabled: false,
+        tiltGesturesEnabled: false,
       ),
     );
   }
