dd992f43791c812bf97aab88e7c4f3c378211329
Nelson Chung <chihchun.chung@gmail.com>
2023-10-14 12:24:14 +0800

    managecase_page 已經可以修改案件，並同步到雲端

--- iPad/management_interface/lib/managecase_page.dart
+++ iPad/management_interface/lib/managecase_page.dart
@@ -3,16 +3,12 @@ import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ManageCasePage extends StatefulWidget {
-  //final String caseId;
-  //ManageCasePage({required this.caseId});
-
   @override
   _ManageCasePageState createState() => _ManageCasePageState();
 }
 
 class _ManageCasePageState extends State<ManageCasePage> {
   List<String> caseIds = []; // 在這裡保存所有獲取的 caseId
-
   DateTime selectedDate = DateTime.now();
   TimeOfDay selectedTime = TimeOfDay.now();
   final DateFormat formatter = DateFormat('yyyy-MM-dd');
@@ -37,11 +33,18 @@ class _ManageCasePageState extends State<ManageCasePage> {
 
   String? currentCaseId;  // 添加這一行來保存當前選擇的 caseId
 
+  Future<Map<String, dynamic>?>? caseDataFuture = Future.value({}); // 初始化
 
   @override
   void initState() {
+    print("Enter initState");
     super.initState();
-    _fetchAllCaseIds();
+    _fetchAllCaseIds().then((_) {
+        if (caseIds.isNotEmpty) {
+            caseDataFuture = _fetchCaseData(caseIds.first);
+            print("caseDataFuture: $caseDataFuture");
+        }
+    });
   }
 
   // 獲取所有 caseId 的函數
@@ -50,15 +53,17 @@ class _ManageCasePageState extends State<ManageCasePage> {
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('travelData').get();
     setState(() {
       caseIds = querySnapshot.docs.map((doc) => doc.id).toList();
+      print("Fetched caseIds: $caseIds");
     });
-    // 如果您想要用第一個 caseId 進行其他操作，您可以這樣做
-    if (caseIds.isNotEmpty) {
-      _fetchCaseData(caseIds.first);  // 使用第一個 caseId 作為例子
-    }
-  }  
+  }
 
-  Future<void> _fetchCaseData(String caseId) async {
+
+  Future<Map<String, dynamic>?> _fetchCaseData(String? caseId) async {
     print("Enter _fetchCaseData");
+
+    if (caseId == null) {
+        return null;
+    }
     currentCaseId = caseId;
 
     final DocumentReference caseRef = FirebaseFirestore.instance.collection('travelData').doc(currentCaseId);
@@ -89,7 +94,9 @@ class _ManageCasePageState extends State<ManageCasePage> {
         dropoffSelectedDistrict = caseData['dropoffSelectedDistrict'];
         dropoffDetailedAddress = caseData['dropoffDetailedAddress'];        
       });
+      return caseData;
     }
+    return null;
   }
 
   Future<void> _updateCaseData(BuildContext context) async {
@@ -140,158 +147,193 @@ class _ManageCasePageState extends State<ManageCasePage> {
       appBar: AppBar(
         title: Text('管理案件'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: <Widget>[
-            // Flight Number
-            TextFormField(
-              initialValue: flightNumber,
-              decoration: InputDecoration(labelText: "航班編號:"),
-              onChanged: (value) {
-                flightNumber = value;
-              },
-            ),
-            // Travel Date
-            ElevatedButton(
-              onPressed: () async {
-                final DateTime? picked = await showDatePicker(
-                  context: context,
-                  initialDate: selectedDate,
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2101),
-                );
-                if (picked != null && picked != selectedDate)
-                  setState(() {
-                    selectedDate = picked;
-                  });
-              },
-              child: Text("選擇日期: ${formatter.format(selectedDate)}"),
-            ),
-            // Travel Time
-            ElevatedButton(
-              onPressed: () async {
-                final TimeOfDay? picked = await showTimePicker(
-                  context: context,
-                  initialTime: selectedTime,
-                );
-                if (picked != null && picked != selectedTime)
-                  setState(() {
-                    selectedTime = picked;
-                  });
-              },
-              child: Text("選擇時間: ${selectedTime.format(context)}"),
-            ),
-            // 上車地點
-            TextFormField(
-            initialValue: pickupSelectedCity,
-            decoration: InputDecoration(labelText: "上車城市:"),
-            onChanged: (value) {
-                pickupSelectedCity = value;
-            },
-            ),
-            TextFormField(
-            initialValue: pickupSelectedDistrict,
-            decoration: InputDecoration(labelText: "上車地區:"),
-            onChanged: (value) {
-                pickupSelectedDistrict = value;
-            },
-            ),
-            TextFormField(
-            initialValue: pickupDetailedAddress,
-            decoration: InputDecoration(labelText: "上車詳細地址:"),
-            onChanged: (value) {
-                pickupDetailedAddress = value;
-            },
-            ),
-            // 目的地
-            TextFormField(
-            initialValue: dropoffSelectedCity,
-            decoration: InputDecoration(labelText: "目的城市:"),
-            onChanged: (value) {
-                dropoffSelectedCity = value;
-            },
-            ),
-            TextFormField(
-            initialValue: dropoffSelectedDistrict,
-            decoration: InputDecoration(labelText: "目的地區:"),
-            onChanged: (value) {
-                dropoffSelectedDistrict = value;
-            },
-            ),
-            TextFormField(
-            initialValue: dropoffDetailedAddress,
-            decoration: InputDecoration(labelText: "目的詳細地址:"),
-            onChanged: (value) {
-                dropoffDetailedAddress = value;
-            },
-            ),
+      //body: Padding(
+////////
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: caseDataFuture, //使用 caseDataFuture
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+          return Center(child: Text("No data"));
+        } else {
+            Map<String, dynamic> caseData = snapshot.data!;
+////////
+            return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView(
+                    children: <Widget>[
+                        // Flight Number
+                        TextFormField(
+                        initialValue: flightNumber,
+                        decoration: InputDecoration(labelText: "航班編號:"),
+                        onChanged: (value) {
+                            flightNumber = value;
+                        },
+                        ),
+                        // Travel Date
+                        ElevatedButton(
+                        onPressed: () async {
+                            final DateTime? picked = await showDatePicker(
+                            context: context,
+                            initialDate: selectedDate,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2101),
+                            );
+                            if (picked != null && picked != selectedDate)
+                            setState(() {
+                                selectedDate = picked;
+                            });
+                        },
+                        child: Text("選擇日期: ${formatter.format(selectedDate)}"),
+                        ),
+                        // Travel Time
+                        ElevatedButton(
+                        onPressed: () async {
+                            final TimeOfDay? picked = await showTimePicker(
+                            context: context,
+                            initialTime: selectedTime,
+                            );
+                            if (picked != null && picked != selectedTime)
+                            setState(() {
+                                selectedTime = picked;
+                            });
+                        },
+                        child: Text("選擇時間: ${selectedTime.format(context)}"),
+                        ),
+                        // 上車地點
+                        TextFormField(
+                        initialValue: pickupSelectedCity,
+                        decoration: InputDecoration(labelText: "上車城市:"),
+                        onChanged: (value) {
+                            pickupSelectedCity = value;
+                        },
+                        ),
+                        TextFormField(
+                        initialValue: pickupSelectedDistrict,
+                        decoration: InputDecoration(labelText: "上車地區:"),
+                        onChanged: (value) {
+                            pickupSelectedDistrict = value;
+                        },
+                        ),
+                        TextFormField(
+                        initialValue: pickupDetailedAddress,
+                        decoration: InputDecoration(labelText: "上車詳細地址:"),
+                        onChanged: (value) {
+                            pickupDetailedAddress = value;
+                        },
+                        ),
+                        // 目的地
+                        TextFormField(
+                        initialValue: dropoffSelectedCity,
+                        decoration: InputDecoration(labelText: "目的城市:"),
+                        onChanged: (value) {
+                            dropoffSelectedCity = value;
+                        },
+                        ),
+                        TextFormField(
+                        initialValue: dropoffSelectedDistrict,
+                        decoration: InputDecoration(labelText: "目的地區:"),
+                        onChanged: (value) {
+                            dropoffSelectedDistrict = value;
+                        },
+                        ),
+                        TextFormField(
+                        initialValue: dropoffDetailedAddress,
+                        decoration: InputDecoration(labelText: "目的詳細地址:"),
+                        onChanged: (value) {
+                            dropoffDetailedAddress = value;
+                        },
+                        ),
 
-            // Number of Passengers
-            TextFormField(
-              initialValue: numberOfPassengers?.toString(),
-              decoration: InputDecoration(labelText: "乘客人數:"),
-              onChanged: (value) {
-                numberOfPassengers = int.parse(value);
-              },
-              keyboardType: TextInputType.number,
-            ),
-            // Number of Luggage
-            TextFormField(
-              initialValue: numberOfLuggage?.toString(),
-              decoration: InputDecoration(labelText: "行李數量:"),
-              onChanged: (value) {
-                numberOfLuggage = int.parse(value);
-              },
-              keyboardType: TextInputType.number,
-            ),
-            // Passenger Name
-            TextFormField(
-              initialValue: passengerName,
-              decoration: InputDecoration(labelText: "乘客名稱:"),
-              onChanged: (value) {
-                passengerName = value;
-              },
-            ),
-            // Contact Number
-            TextFormField(
-              initialValue: contactNumber,
-              decoration: InputDecoration(labelText: "聯絡號碼:"),
-              onChanged: (value) {
-                contactNumber = value;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-            // Remarks
-            TextFormField(
-              initialValue: remarks,
-              decoration: InputDecoration(labelText: "備註:"),
-              onChanged: (value) {
-                remarks = value;
-              },
-            ),
-            // Driver ID
-            TextFormField(
-              initialValue: driverID,
-              decoration: InputDecoration(labelText: "司機ID:"),
-              onChanged: (value) {
-                driverID = value;
-              },
-            ),
-            // Status Case
-            TextFormField(
-              initialValue: statusCase,
-              decoration: InputDecoration(labelText: "案件狀態:"),
-              onChanged: (value) {
-                statusCase = value;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () => _updateCaseData(context),
-              child: Text('更新案件'),
-            ),
-          ],
-        ),
+                        // Number of Passengers
+                        TextFormField(
+                        initialValue: numberOfPassengers?.toString(),
+                        decoration: InputDecoration(labelText: "乘客人數:"),
+                        onChanged: (value) {
+                            numberOfPassengers = int.parse(value);
+                        },
+                        keyboardType: TextInputType.number,
+                        ),
+                        // Number of Luggage
+                        TextFormField(
+                        initialValue: numberOfLuggage?.toString(),
+                        decoration: InputDecoration(labelText: "行李數量:"),
+                        onChanged: (value) {
+                            numberOfLuggage = int.parse(value);
+                        },
+                        keyboardType: TextInputType.number,
+                        ),
+                        // Passenger Name
+                        TextFormField(
+                        initialValue: passengerName,
+                        decoration: InputDecoration(labelText: "乘客名稱:"),
+                        onChanged: (value) {
+                            passengerName = value;
+                        },
+                        ),
+                        // Contact Number
+                        TextFormField(
+                        initialValue: contactNumber,
+                        decoration: InputDecoration(labelText: "聯絡號碼:"),
+                        onChanged: (value) {
+                            contactNumber = value;
+                        },
+                        keyboardType: TextInputType.phone,
+                        ),
+                        // Remarks
+                        TextFormField(
+                        initialValue: remarks,
+                        decoration: InputDecoration(labelText: "備註:"),
+                        onChanged: (value) {
+                            remarks = value;
+                        },
+                        ),
+                        // Driver ID
+                        DropdownButtonFormField<String>(
+                        value: driverID,
+                        decoration: InputDecoration(labelText: "司機ID:"),
+                        onChanged: (String? newValue) {
+                            setState(() {
+                            driverID = newValue;
+                            });
+                        },
+                        items: <String>['','driver1', 'driver2']
+                            .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                            );
+                        }).toList(),
+                        ),
+                        // Status Case
+                        DropdownButtonFormField<String>(
+                        value: statusCase,
+                        decoration: InputDecoration(labelText: "案件狀態:"),
+                        onChanged: (String? newValue) {
+                            setState(() {
+                            statusCase = newValue;
+                            });
+                        },
+                        items: <String>[
+                            '正在安排司機',
+                            '已安排好司機',
+                            '載送中',
+                            '已結案'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                            );
+                        }).toList(),
+                        ),                    
+                    ],
+                ),
+              );
+            }
+        },
       ),
     );
   }
